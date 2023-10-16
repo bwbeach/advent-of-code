2442a9cc946514567480d614a34a43405df26ac9
Brian Beach <coder@beachfamily.net>
2023-10-16 15:17:23 -0400

    part 2

--- 2020/day02/app/Main.hs
+++ 2020/day02/app/Main.hs
@@ -4,7 +4,7 @@ import Advent (runTestAndInput)
 import Data.List.Split (splitOn)
 
 main :: IO ()
-main = runTestAndInput parse id id
+main = runTestAndInput parse (solve isValid1) (solve isValid2)
 
 -- | Parses puzzle input line by line.
 parse :: String -> [(Int, Int, Char, String)]
@@ -22,3 +22,21 @@ parseLine s =
     [nums, middle, p] = splitOn " " s
     c = head middle
     [a, b] = map read . splitOn "-" $ nums
+
+-- | Runs day02 on one input
+solve :: ((Int, Int, Char, String) -> Bool) -> [(Int, Int, Char, String)] -> Int
+solve isValid = length . filter isValid
+
+-- | Is one password valid in part 1?
+isValid1 :: (Int, Int, Char, String) -> Bool
+isValid1 (a, b, c, p) =
+  a <= n && n <= b
+  where
+    n = length . filter (== c) $ p
+
+-- | Is one password valid in part 2?
+isValid2 :: (Int, Int, Char, String) -> Bool
+isValid2 (a, b, c, p) =
+  xor ((p !! (a - 1)) == c) ((p !! (b - 1)) == c)
+  where
+    xor a b = (a && not b) || (not a && b)
