7679d8cb13d6ed4c6494d38a96ed00311ade7866
Brian Beach <coder@beachfamily.net>
2023-10-23 20:36:04 -0400

    Add first unit test to advent library

--- libs/advent/src/Advent.hs
+++ libs/advent/src/Advent.hs
@@ -1,4 +1,9 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+
 module Advent (runTestAndInput) where
+
+import Data.Map.Strict qualified as M
+import Linear.V2 (V2 (..))
 
 -- | Runs a solution for bath parts of one day against both test and input files.
 runTestAndInput :: (Show b) => (String -> a) -> (a -> b) -> (a -> b) -> IO ()
@@ -12,3 +17,26 @@ runTestAndInput parse part1 part2 = do
       let input = parse text
       print . part1 $ input
       print . part2 $ input
+
+-- | A grid of characters.
+--
+-- Advent of Code problems frequently start as grids of characters.
+-- The problems usualy index them 1-based, with (1, 1) at the upper
+-- left, positive x going right, and positive y going down.
+--
+-- A grid is stored as a map from position to non-space character at
+-- that position.
+newtype Grid = Grid (M.Map (V2 Int) Char) deriving (Eq)
+
+-- | Extracts the map from a Grid
+gridMap :: Grid -> M.Map (V2 Int) Char
+gridMap (Grid m) = m
+
+-- instance Show Grid where
+--  shows g _ = "g"
+
+-- | Converts lines of characters to a grid.
+--
+-- The grid is represented as a map from (V2 x y) to a non-space
+-- character at that position.
+-- instance Read Grid where
--- libs/advent/test/AdventTest.hs
+++ libs/advent/test/AdventTest.hs
@@ -1,4 +1,14 @@
 module Main (main) where
 
+import System.Exit
+import Test.HUnit
+
 main :: IO ()
-main = putStrLn "Tests not implemented"
+main = do
+  result <- runTestTT tests
+  if failures result > 0 then exitFailure else exitSuccess
+
+test1 :: Test
+test1 = TestCase (assertEqual "one" 1 1)
+
+tests = TestList [TestLabel "test1" test1]
