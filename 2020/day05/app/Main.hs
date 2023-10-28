a46ca0611ebd0d253e15766bd726072f761a39ad
Brian Beach <coder@beachfamily.net>
2023-10-28 19:48:58 -0400

    factor out seatId

--- 2020/day05/app/Main.hs
+++ 2020/day05/app/Main.hs
@@ -10,11 +10,15 @@ parse :: String -> [String]
 parse = endBy "\n"
 
 part1 :: [String] -> Int
-part1 =
-  maximum . map seatId
-  where
-    seatId = foldl go 0
-    go n c = n * 2 + charVal c
-    charVal c = if c `elem` "RB" then 1 else 0
+part1 = maximum . map seatId
 
 part2 _ = 5
+
+-- | Converts a boarding pass code to a Seat ID
+-- The codes are binary once the characters are converted to ones and zeros.
+seatId :: String -> Int
+seatId =
+  foldl go 0
+  where
+    go n c = n * 2 + charVal c
+    charVal c = if c `elem` "RB" then 1 else 0
