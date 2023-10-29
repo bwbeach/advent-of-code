8186866b17804e79ca2114680d09580dbe927d52
Brian Beach <coder@beachfamily.net>
2023-10-29 14:13:13 -0400

    2020 day 7 parsing

--- 2020/day07/app/Main.hs
+++ 2020/day07/app/Main.hs
@@ -1,17 +1,46 @@
 module Main where
 
 import Advent (runTestAndInput)
+import Data.List.Split
 
 main :: IO ()
 main = runTestAndInput parse part1 part2
 
-type Problem = String
+type Color = String
+
+type Rule = (Color, [(Int, Color)])
+
+type Problem = [Rule]
 
 parse :: String -> Problem
-parse = id
+parse =
+  map parseLine . endBy "\n"
 
-part1 :: Problem -> Int
-part1 = length
+-- | Parse one input line
+-- Examples:
+--    dotted black bags contain no other bags.
+--    vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+parseLine :: String -> Rule
+parseLine line =
+  (bagColor left, parseRhs right)
+  where
+    parseRhs "no other bags." = []
+    parseRhs s = map parseContent . splitOn "," $ s
+
+    parseContent s =
+      (count, color)
+      where
+        count = read (head ws)
+        color = unwords . take 2 . drop 1 $ ws
+        ws = words s
+
+    [left, right] = splitOn " contain " line
+
+    -- "bright red bags" -> "bright red"
+    bagColor = unwords . take 2 . words
+
+part1 :: Problem -> Problem
+part1 = id
 
 part2 :: Problem -> Int
 part2 = length
