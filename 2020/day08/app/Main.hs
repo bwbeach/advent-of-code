7b3d4f97a4616431a40a54b2788a8d514a015331
Brian Beach <coder@beachfamily.net>
2023-11-01 21:30:10 -0400

    2020 day 9 part 1

--- 2020/day08/app/Main.hs
+++ 2020/day08/app/Main.hs
@@ -3,6 +3,9 @@ module Main where
 import Advent (runTestAndInput)
 import Data.List.Split (endBy)
 import qualified Data.Map.Strict as M
+import Data.Maybe (fromJust)
+import qualified Data.Set as S
+import Debug.Trace
 
 main :: IO ()
 main = runTestAndInput parse part1 part2
@@ -30,8 +33,30 @@ parse =
     parseNum ('+' : n) = read n
     parseNum n = read n
 
+-- | The state of the computer is (addr, acc)
+-- The addr is the next instruction to run, and the acc is the accumulator
+-- just before running that instruction.
+type State = (Int, Int)
+
+-- | Runs the next instruction, producing the new state.
+step :: Problem -> State -> State
+step problem (addr, acc) =
+  runInstruction . fromJust $ M.lookup addr problem
+  where
+    runInstruction (Acc n) = (addr + 1, acc + n)
+    runInstruction (Jmp n) = (addr + n, acc)
+    runInstruction Nop = (addr + 1, acc)
+
 part1 :: Problem -> Int
-part1 = length
+part1 problem =
+  go S.empty (1, 0)
+  where
+    go visited (addr, acc) =
+      if S.member addr visited
+        then acc
+        else go (S.insert addr visited) (addr', acc')
+      where
+        (addr', acc') = step problem (addr, acc)
 
 part2 :: Problem -> Int
 part2 = length
