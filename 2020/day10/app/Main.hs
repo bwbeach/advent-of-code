779869fd7205cf1ad30932303aac18b554cab4f9
Brian Beach <coder@beachfamily.net>
2023-11-03 20:37:18 -0400

    2020 day 10 part 2

--- 2020/day10/app/Main.hs
+++ 2020/day10/app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Advent (runTestAndInput)
 import Data.List (group, sort)
 import qualified Data.Map.Strict as M
+import Data.Maybe (fromMaybe)
 import Debug.Trace
 import Topograph (pairs)
 
@@ -23,5 +24,15 @@ part1 problem =
     valueAndCount as = (head as, length as)
     delta (a, b) = b - a
 
-part2 :: Problem -> Int
-part2 = length
+part2 :: Problem -> Integer
+part2 problem =
+  answers M.! 0
+  where
+    answers = foldr step (M.singleton top 1) (0 : sort problem)
+
+    step :: Int -> M.Map Int Integer -> M.Map Int Integer
+    step n soFar = M.insert n (sum . map (getPossible soFar) $ [n + 1 .. n + 3]) soFar
+
+    getPossible soFar n = M.findWithDefault 0 n soFar
+
+    top = 3 + maximum problem
