53180e094f30965c68d27eb21f1a901980a4c037
Brian Beach <coder@beachfamily.net>
2023-11-18 09:55:15 -0500

    Add script for creating a project.

---
+++ new-day.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+# Fail if any command fails
+set -e 
+
+# Expect one arg that is the name of the directory/project to create.
+if [[ -z "$1" ]]; then
+    echo "Usage: $0 <dirToCreate>"
+    exit 1
+fi
+
+# Create the directory
+if [[ -e "$1" ]]; then
+    echo "$1 already exists"
+    exit 1
+fi
+mkdir $1
+
+# Get the root of the git project
+gitroot=$(git rev-parse --show-toplevel)
+if [[ ! -e $gitroot/templates/cabal.project ]]; then
+    echo "could not find templates"
+    exit 1
+fi
+
+# Initialize the Haskell project with cabal
+cd $1
+cabal init
+
+# Copy in templates
+cp -v $gitroot/templates/cabal.project .
+cp -v $gitroot/templates/Main.hs app
+cp -v $gitroot/templates/answers.txt .
+
+# Add dependency on advent library
+echo "Adding dependency on advent"
+sed -i "" -e '/build-depends/s/$/, advent/' $1.cabal
