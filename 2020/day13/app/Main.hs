349295555e1890b61634397307ca49103d1737f4
Brian Beach <coder@beachfamily.net>
2023-11-19 09:53:59 -0500

    2020 day13 part 1

--- 2020/day13/app/Main.hs
+++ 2020/day13/app/Main.hs
@@ -1,17 +1,36 @@
 module Main where
 
 import Advent (run)
+import Data.List.Split (splitOn)
+import Data.Maybe (mapMaybe)
 
 main :: IO ()
 main = run parse part1 part2
 
-type Problem = String
+data BusId
+  = X
+  | Id Int
+  deriving (Eq, Show)
+
+type Problem = (Int, [BusId])
 
 parse :: String -> Problem
-parse = id
+parse text =
+  (earliest, buses)
+  where
+    [line1, line2] = lines text
+    earliest = read line1
+    buses = map parseBus . splitOn "," $ line2
+    parseBus "x" = X
+    parseBus ns = Id (read ns)
 
 part1 :: Problem -> Int
-part1 = length
+part1 (earliest, buses) =
+  (t - earliest) * i
+  where
+    (t, i) = minimum . mapMaybe arrival $ buses
+    arrival X = Nothing
+    arrival (Id bid) = Just ((earliest `div` bid) * bid + bid, bid)
 
 part2 :: Problem -> Int
 part2 = length
