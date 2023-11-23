0d581e4a3c3e6baf85431fadf7f437bf0472b07c
Brian Beach <coder@beachfamily.net>
2023-11-23 09:45:11 -0500

    run-all.sh runs doc tests

--- run-all.sh
+++ run-all.sh
@@ -1,5 +1,35 @@
 #! /bin/bash
 
+for d in libs/*
+do
+    echo
+    echo ======================================== 
+    echo =
+    echo = $d
+    echo =
+    echo ======================================== 
+    echo
+    echo $d
+    pushd $d
+    cabal repl --with-ghc=doctest
+    if [[ $? -eq 0 ]]
+    then
+       echo DOCTEST SUCCESS
+    else
+       echo DOCTEST FAIL
+       exit 1
+    fi
+    cabal test
+    if [[ $? -eq 0 ]]
+    then
+        echo SUCCESS
+    else
+        echo FAILED
+        exit 1
+    fi
+    popd
+done
+
 for d in 2*/day*
 do
     echo
@@ -11,6 +41,14 @@ do
     echo
     echo $d
     pushd $d
+    cabal repl --with-ghc=doctest
+    if [[ $? -eq 0 ]]
+    then
+       echo DOCTEST SUCCESS
+    else
+       echo DOCTEST FAIL
+       exit 1
+    fi
     cabal run
     if [[ $? -eq 0 ]]
     then
