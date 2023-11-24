f72e536b07340b13c82dc57ab23c77ca948cd735
Brian Beach <coder@beachfamily.net>
2023-11-23 20:34:58 -0500

    2020 day 17 part 1

--- 2020/day17/app/Main.hs
+++ 2020/day17/app/Main.hs
@@ -1,15 +1,15 @@
 module Main where
 
 import Advent
-  ( LifeGrid (lgCandidates, lgCells, lgGet, lgNew),
-    Point,
+  ( Point,
     gridMap,
     gridParse,
     run,
   )
 import Data.List (intercalate)
 import qualified Data.Map.Strict as M
-import Data.Maybe (fromMaybe)
+import Data.Maybe (fromMaybe, mapMaybe, maybeToList)
+import qualified Data.Set as S
 import Data.Tuple.Extra (first)
 import Debug.Trace
 import Linear.V2 (V2 (..))
@@ -32,10 +32,28 @@ point3Y (V3 _ y _) = y
 point3Z :: V3 a -> a
 point3Z (V3 _ _ z) = z
 
-type Grid3 = M.Map Point3 Char
+point3Neighbors :: (Num a, Eq a) => V3 a -> [V3 a]
+point3Neighbors p =
+  [ p + V3 dx dy dz
+    | dx <- [-1, 0, 1],
+      dy <- [-1, 0, 1],
+      dz <- [-1, 0, 1],
+      dx /= 0 || dy /= 0 || dz /= 0
+  ]
+
+newtype Grid3 = Grid3 (M.Map Point3 Char)
+
+grid3Map :: Grid3 -> M.Map Point3 Char
+grid3Map (Grid3 m) = m
+
+grid3Get :: V3 Int -> Grid3 -> Maybe Char
+grid3Get p (Grid3 m) = M.lookup p m
+
+grid3Points :: Grid3 -> [Point3]
+grid3Points (Grid3 m) = M.keys m
 
 grid3Bounds :: Grid3 -> (Point3, Point3)
-grid3Bounds g =
+grid3Bounds (Grid3 g) =
   (V3 (minimum xs) (minimum ys) (minimum zs), V3 (maximum xs) (maximum ys) (maximum zs))
   where
     ps = M.keys g
@@ -44,10 +62,10 @@ grid3Bounds g =
     zs = map point3Z ps
 
 grid3Format :: Grid3 -> String
-grid3Format g =
-  intercalate "\n" . map formatSlice $ [z0 .. z1]
+grid3Format (Grid3 g) =
+  (++ "\n--------\n\n") . intercalate "\n" . map formatSlice $ [z0 .. z1]
   where
-    (V3 x0 y0 z0, V3 x1 y1 z1) = grid3Bounds g
+    (V3 x0 y0 z0, V3 x1 y1 z1) = grid3Bounds (Grid3 g)
     formatSlice z = unlines . map (formatRow z) $ [y0 .. y1]
     formatRow z y = map (formatCell z y) [x0 .. x1]
     formatCell z y x = fromMaybe '.' (M.lookup (V3 x y z) g)
@@ -56,18 +74,38 @@ type Problem = Grid3
 
 parse :: String -> Problem
 parse =
-  M.fromList . map (first point3From2) . M.toList . gridMap . gridParse . replace '.' ' '
+  Grid3 . M.fromList . map (first point3From2) . M.toList . gridMap . gridParse . replace '.' ' '
   where
     replace a b = map (\x -> if x == a then b else x)
 
 part1 :: Problem -> Int
-part1 = length . M.toList
+part1 = length . M.toList . grid3Map . (!! 6) . conway3
 
-instance LifeGrid Grid3 where
-  lgNew _ = M.fromList
-  lgGet = M.lookup
-  lgCells = M.keys
-  lgCandidates = grid3Candidates
+conway3 :: Grid3 -> [Grid3]
+conway3 = iterate step3
+
+step3 :: Grid3 -> Grid3
+step3 g =
+  Grid3 . M.fromList $ result
+  where
+    result =
+      [ (p, c')
+        | p <- candidates g,
+          let c = get p,
+          let ns = mapMaybe get . point3Neighbors $ p,
+          c' <- maybeToList $ rule c ns
+      ]
+    candidates = uniqueValues . concatMap selfAndNeighbors . grid3Points
+    get :: Point3 -> Maybe Char
+    get p = grid3Get p g
+    uniqueValues = S.toList . S.fromList
+    selfAndNeighbors p = p : point3Neighbors p
+
+rule :: Maybe Char -> [Char] -> Maybe Char
+rule (Just '#') "##" = Just '#'
+rule (Just '#') "###" = Just '#'
+rule Nothing "###" = Just '#'
+rule _ _ = Nothing
 
 part2 :: Problem -> Int
-part2 = length
+part2 = length . M.toList . grid3Map
