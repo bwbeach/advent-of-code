8d09c1c5cac08483649575f8cc204531c220f967
Brian Beach <coder@beachfamily.net>
2023-11-28 20:44:17 -0500

    2020 day 20 parsing

--- 2020/day20/app/Main.hs
+++ 2020/day20/app/Main.hs
@@ -1,17 +1,32 @@
 module Main where
 
-import Advent (run)
+import Advent
+  ( Grid (..),
+    gridParse,
+    only,
+    run,
+  )
+import Data.List.Extra (dropEnd)
+import Data.List.Split (splitOn)
+import qualified Data.Map.Strict as M
 
 main :: IO ()
 main = run parse part1 part2
 
-type Problem = String
+type Problem = M.Map Integer Grid
 
 parse :: String -> Problem
-parse = id
+parse = M.fromList . map parseTile . splitOn "\n\n"
+
+parseTile :: String -> (Integer, Grid)
+parseTile s =
+  (number, grid)
+  where
+    number = read . dropEnd 1 . (!! 1) . words . head . lines $ s
+    grid = gridParse . unlines . tail . lines $ s
 
 part1 :: Problem -> Int
-part1 = length
+part1 = M.size
 
 part2 :: Problem -> Int
 part2 = length
