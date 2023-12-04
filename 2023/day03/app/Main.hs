fb3b29ccc9642c61e0d4d2b1ec6b1aa10c47fa44
Brian Beach <coder@beachfamily.net>
2023-12-04 03:00:12 +0000

    reformat

--- 2023/day03/app/Main.hs
+++ 2023/day03/app/Main.hs
@@ -2,10 +2,10 @@ module Main where
 
 import Advent (Grid (..), Point, gridGet, gridMap, gridParse, run)
 import Data.Char (isDigit)
-import Data.Maybe (fromMaybe, mapMaybe)
-import Linear.V2 (V2 (..))
 import qualified Data.Map.Strict as M
+import Data.Maybe (fromMaybe, mapMaybe)
 import qualified Data.Set as S
+import Linear.V2 (V2 (..))
 
 main :: IO ()
 main = run parse part1 part2
@@ -13,58 +13,60 @@ main = run parse part1 part2
 type Problem = Grid
 
 parse :: String -> Problem
-parse = gridParse . replace '.' ' ' 
+parse = gridParse . replace '.' ' '
 
-replace :: Eq a => a -> a -> [a] -> [a] 
+replace :: (Eq a) => a -> a -> [a] -> [a]
 replace a b = map (\x -> if x == a then b else x)
 
 part1 :: Problem -> Int
 part1 g = sum . map (readPartNumber g) . findPartNumbers $ g
 
 part2 :: Problem -> Int
-part2 g = 
-    sum . mapMaybe starToMaybeRatio $ stars
-    where 
-        starToMaybeRatio p = 
-            if length neighboringNumbers == 2 
-                then Just (product neighboringNumbers)
-                else Nothing
-            where 
-                neighboringNumbers = starToNeigboringNumbers p
-        starToNeigboringNumbers p = 
-            map (readPartNumber g) . filter (any (`elem` starNeighbors)) $ partNumbers
-            where 
-                starNeighbors = neighbors p
-        partNumbers = findPartNumbers g
-        stars = filter ((== '*') . getGridPoint g) points 
-        points = M.keys . gridMap $ g
-
+part2 g =
+  sum . mapMaybe starToMaybeRatio $ stars
+  where
+    starToMaybeRatio p =
+      if length neighboringNumbers == 2
+        then Just (product neighboringNumbers)
+        else Nothing
+      where
+        neighboringNumbers = starToNeigboringNumbers p
+    starToNeigboringNumbers p =
+      map (readPartNumber g) . filter (any (`elem` starNeighbors)) $ partNumbers
+      where
+        starNeighbors = neighbors p
+    partNumbers = findPartNumbers g
+    stars = filter ((== '*') . getGridPoint g) points
+    points = M.keys . gridMap $ g
 
 findPartNumbers :: Grid -> [[Point]]
 findPartNumbers g =
-    filter isPartNumber . map pointsOfNumber . filter isStartOfNumber $ points 
-    where
-        points = M.keys . gridMap $ g
-        isStartOfNumber p = gridHasDigit p && not (gridHasDigit (left p))
-        gridHasDigit p = isDigit . gridGet p  $ g
-        pointsOfNumber = takeWhile gridHasDigit . iterate right
-        isPartNumber = any (isSymbol . get) . neighborsOfNumber
-        isSymbol c = not (isDigit c || c == ' ')
-        get = getGridPoint g
-        neighborsOfNumber = S.toList . S.fromList . concatMap neighbors
+  filter isPartNumber . map pointsOfNumber . filter isStartOfNumber $ points
+  where
+    points = M.keys . gridMap $ g
+    isStartOfNumber p = gridHasDigit p && not (gridHasDigit (left p))
+    gridHasDigit p = isDigit . gridGet p $ g
+    pointsOfNumber = takeWhile gridHasDigit . iterate right
+    isPartNumber = any (isSymbol . get) . neighborsOfNumber
+    isSymbol c = not (isDigit c || c == ' ')
+    get = getGridPoint g
+    neighborsOfNumber = S.toList . S.fromList . concatMap neighbors
 
-readPartNumber :: Grid -> [Point] -> Int 
+readPartNumber :: Grid -> [Point] -> Int
 readPartNumber g = read . map (getGridPoint g)
 
-getGridPoint :: Grid -> Point -> Char 
+getGridPoint :: Grid -> Point -> Char
 getGridPoint g p = gridGet p g
 
 neighbors :: Point -> [Point]
-neighbors p = 
-    [ p + V2 dx dy |
-      dx <- [(-1) .. 1],
+neighbors p = filter (/= p) . neighbors $ p
+
+neighborsAndSelf :: Point -> [Point] 
+neighborsAndSelf p =
+  [ p + V2 dx dy
+    | dx <- [(-1) .. 1],
       dy <- [(-1) .. 1]
-    ]
+  ]
 
 left :: Point -> Point
 left p = p + V2 (-1) 0
