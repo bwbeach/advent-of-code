e11622dcbeff8dc9d5d95f4f66413cc819187021
Brian Beach <coder@beachfamily.net>
2023-12-04 05:14:54 +0000

    2023 day 4 part 1

--- 2023/day04/app/Main.hs
+++ 2023/day04/app/Main.hs
@@ -1,17 +1,40 @@
 module Main where
 
 import Advent (run)
+import qualified Data.Set as S
+import Data.List.Split (splitOn)
 
 main :: IO ()
 main = run parse part1 part2
 
-type Problem = String
+type Card = (Int, S.Set Int, S.Set Int)
 
+type Problem = [Card]
+
+-- 
 parse :: String -> Problem
-parse = id
+parse = map parseLine . lines
+
+-- | Parses one line that looks like this:
+-- Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+parseLine :: String -> Card 
+parseLine s =
+    (read . drop 5 $ a, winners, have)
+    where 
+        [a, b] = splitOn ": " s
+        [c, d] = splitOn " | " b 
+        winners = S.fromList . map read . words $ c 
+        have = S.fromList . map read . words $ d
 
 part1 :: Problem -> Int
-part1 = length
+part1 = 
+    sum . map cardScore 
+    where 
+         cardScore (_, w, h) = exp2 . length . filter (`S.member` w) . S.toList $ h 
+
+         exp2 0 = 0
+         exp2 1 = 1
+         exp2 n = 2 * exp2 (n - 1)
 
 part2 :: Problem -> Int
 part2 = length
