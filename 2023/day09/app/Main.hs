347c378f9e16f9646c0c6f9456f090959191fd3c
Brian Beach <coder@beachfamily.net>
2023-12-10 11:05:39 -0500

    2023 day 9 part 1

--- 2023/day09/app/Main.hs
+++ 2023/day09/app/Main.hs
@@ -1,17 +1,29 @@
 module Main where
 
 import Advent (run)
+import Data.Tuple (swap)
+import Topograph (pairs)
 
 main :: IO ()
 main = run parse part1 part2
 
-type Problem = String
+type Problem = [[Int]]
 
 parse :: String -> Problem
-parse = id
+parse = map parseLine . lines
+  where
+    parseLine = map read . words
 
 part1 :: Problem -> Int
-part1 = length
+part1 = sum . map extrapolateOne
+
+extrapolateOne :: [Int] -> Int
+extrapolateOne ns =
+  if all (== 0) ns
+    then 0
+    else last ns + extrapolateOne (differences ns)
+  where
+    differences = map (uncurry (-) . swap) . pairs
 
 part2 :: Problem -> Int
 part2 = length
