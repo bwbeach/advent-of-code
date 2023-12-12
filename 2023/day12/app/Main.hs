0b59ee42b474348e16ba3400ab9da66d5cf9d282
Brian Beach <coder@beachfamily.net>
2023-12-12 17:52:44 -0500

    2023 day 12 part 1

--- 2023/day12/app/Main.hs
+++ 2023/day12/app/Main.hs
@@ -1,17 +1,68 @@
 module Main where
 
 import Advent (run)
+import Data.List (intercalate)
+import Data.List.Split (splitOn)
+import Data.Tuple.Extra (second)
+import Debug.Trace
 
 main :: IO ()
 main = run parse part1 part2
 
-type Problem = String
+type Problem = [(String, [Int])]
 
 parse :: String -> Problem
-parse = id
+parse =
+  map parseLine . lines
+  where
+    parseLine s =
+      (a, ns)
+      where
+        [a, b] = splitOn " " s
+        ns = map read . splitOn "," $ b
 
 part1 :: Problem -> Int
-part1 = length
+part1 =
+  sum . map runOne
+  where
+    runOne (s, ns) = length $ allMatches s (compilePattern ns)
+
+data PatternElem
+  = Dot -- exactly one '.'
+  | Dots -- zero or more '.'
+  | Hash -- exactly one '#'
+  deriving (Eq, Show)
+
+-- | Translate the list of numbers from the problem into a pattern
+--
+-- >>> compilePattern [2, 1]
+-- [Dots,Hash,Hash,Dot,Dots,Hash,Dots]
+compilePattern :: [Int] -> [PatternElem]
+compilePattern ns = [Dots] ++ intercalate [Dot, Dots] (map (`replicate` Hash) ns) ++ [Dots]
+
+-- | Return all of the strings matching the input string and the pattern
+--
+-- >>> allMatches "" []
+-- [""]
+--
+-- >>> allMatches "?" [Hash]
+-- ["#"]
+--
+-- >>> allMatches "???.###" [Dots, Hash, Dot, Dots, Hash, Dot, Dots, Hash, Hash, Hash, Dots]
+-- ["#.#.###"]
+--
+--
+-- >>> allMatches ".??..??...?##." [Dots, Hash, Dot, Dots, Hash, Dot, Dots, Hash, Hash, Hash, Dots]
+-- WAS []
+-- NOW [".#...#....###.",".#....#...###.","..#..#....###.","..#...#...###."]
+allMatches :: String -> [PatternElem] -> [String]
+allMatches "" [] = [""]
+allMatches ('.' : cs) (Dot : ps) = map ('.' :) (allMatches cs ps)
+allMatches ('?' : cs) (Dot : ps) = map ('.' :) (allMatches cs ps)
+allMatches ('#' : cs) (Hash : ps) = map ('#' :) (allMatches cs ps)
+allMatches ('?' : cs) (Hash : ps) = map ('#' :) (allMatches cs ps)
+allMatches cs (Dots : ps) = allMatches cs ps ++ allMatches cs (Dot : Dots : ps)
+allMatches _ _ = []
 
 part2 :: Problem -> Int
 part2 = length
