2fee05b0599c2c29c5e28848ef4c5d5906a9ce4c
Brian Beach <coder@beachfamily.net>
2023-12-13 10:49:41 -0500

    2023 day 13 part 1

--- 2023/day13/app/Main.hs
+++ 2023/day13/app/Main.hs
@@ -1,17 +1,43 @@
 module Main where
 
 import Advent (run)
+import Data.List (transpose)
+import Data.List.Split (splitOn)
 
 main :: IO ()
 main = run parse part1 part2
 
-type Problem = String
+-- | A list of the rows of text in one grid of ash ('.') and rocks ('#')
+type Pattern = [String]
+
+type Problem = [Pattern]
 
 parse :: String -> Problem
-parse = id
+parse = splitOn [""] . lines
 
 part1 :: Problem -> Int
-part1 = length
+part1 =
+  sum . map summary
+  where
+    summary p = 100 * reflectionSum p + reflectionSum (transpose p)
+
+-- | The sum of reflection scores
+reflectionSum :: (Eq a) => [a] -> Int
+reflectionSum =
+  sum . map (length . fst) . filter (uncurry isReflection) . candidates
+  where
+    isReflection as@(_ : _) bs@(_ : _) = all (uncurry (==)) $ zip as bs
+    isReflection _ _ = False
+
+-- | All of the candidate pairs that might be reflections.
+candidates :: (Eq a) => [a] -> [([a], [a])]
+candidates as =
+  go [] as
+  where
+    go revBefore after = (revBefore, after) : go' revBefore after
+
+    go' _ [] = []
+    go' revBefore (a : as) = go (a : revBefore) as
 
 part2 :: Problem -> Int
 part2 = length
