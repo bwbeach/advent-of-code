34259eeb225aeff0007141b30d48d668ce6eaab6
Brian Beach <coder@beachfamily.net>
2023-12-15 18:16:13 -0500

    2023 day 13 part 2

--- 2023/day13/app/Main.hs
+++ 2023/day13/app/Main.hs
@@ -3,6 +3,8 @@ module Main where
 import Advent (run)
 import Data.List (transpose)
 import Data.List.Split (splitOn)
+import Debug.Trace
+import GHC.Event.TimeOut (TimeoutKey (TK))
 
 main :: IO ()
 main = run parse part1 part2
@@ -16,21 +18,41 @@ parse :: String -> Problem
 parse = splitOn [""] . lines
 
 part1 :: Problem -> Int
-part1 =
+part1 = goPart 0
+
+part2 :: Problem -> Int
+part2 = goPart 1
+
+goPart :: Int -> Problem -> Int
+goPart expectedSmudges =
   sum . map summary
   where
-    summary p = 100 * reflectionSum p + reflectionSum (transpose p)
+    summary p = 100 * sumOne p + sumOne (transpose p)
+    sumOne = reflectionSum expectedSmudges
 
 -- | The sum of reflection scores
-reflectionSum :: (Eq a) => [a] -> Int
-reflectionSum =
-  sum . map (length . fst) . filter (uncurry isReflection) . candidates
+reflectionSum :: Int -> [String] -> Int
+reflectionSum expectedSmudges =
+  sum . map (length . fst) . filter ((== expectedSmudges) . smudgeCount) . filter bothNonEmpty . candidates
   where
-    isReflection as@(_ : _) bs@(_ : _) = all (uncurry (==)) $ zip as bs
+    bothNonEmpty (_ : _, _ : _) = True
+    bothNonEmpty (_, _) = False
+
+    isReflection as@(_ : _) bs@(_ : _) = and $ zipWith (==) as bs
     isReflection _ _ = False
 
+-- | Count the smudges for a reflection candidate
+--
+-- >>> smudgeCount (["#.#", "..#"], ["#..", ".##"])
+-- 2
+smudgeCount :: ([String], [String]) -> Int
+smudgeCount (as, bs) =
+  sum $ zipWith rowCount as bs
+  where
+    rowCount a b = length . filter id $ zipWith (/=) a b
+
 -- | All of the candidate pairs that might be reflections.
-candidates :: (Eq a) => [a] -> [([a], [a])]
+candidates :: [String] -> [([String], [String])]
 candidates as =
   go [] as
   where
@@ -38,6 +60,3 @@ candidates as =
 
     go' _ [] = []
     go' revBefore (a : as) = go (a : revBefore) as
-
-part2 :: Problem -> Int
-part2 = length
