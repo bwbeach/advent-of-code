7a4d96f0aeed40b8f063783095e771f873b16446
Brian Beach <coder@beachfamily.net>
2023-12-16 09:20:46 -0500

    2023 day 15 part 1

--- 2023/day15/app/Main.hs
+++ 2023/day15/app/Main.hs
@@ -1,17 +1,26 @@
 module Main where
 
 import Advent (run)
+import Data.Char (ord)
+import Data.List (foldl')
+import Data.List.Split (splitOn)
 
 main :: IO ()
 main = run parse part1 part2
 
-type Problem = String
+type Problem = [String]
 
 parse :: String -> Problem
-parse = id
+parse = splitOn "," . filter (/= '\n')
 
 part1 :: Problem -> Int
-part1 = length
+part1 = sum . map hash
 
 part2 :: Problem -> Int
 part2 = length
+
+hash :: String -> Int
+hash =
+  foldl' hashChar 0
+  where
+    hashChar h c = ((h + ord c) * 17) `mod` 256
