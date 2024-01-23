6a1ea4eecd81c7bad3aa049f31de82a2432f3bd8
Brian Beach <coder@beachfamily.net>
2024-01-22 17:50:11 -1000

    fix part 2

--- 2020/day22/app/Main.hs
+++ 2020/day22/app/Main.hs
@@ -2,6 +2,8 @@ module Main where
 
 import Advent (run)
 import Data.List.Split (splitOn)
+import qualified Data.Set as S
+import Debug.Trace
 
 main :: IO ()
 main = run parse part1 part2
@@ -44,10 +46,62 @@ iterateMaybe f a =
 
 part1 :: Problem -> Int
 part1 =
-  score . last . iterateMaybe oneRound
+  scoreHands . last . iterateMaybe oneRound
+
+-- | Return the score for a pair of hands
+scoreHands :: (Hand, Hand) -> Int
+scoreHands (a, b) =
+  if null a then scoreHand b else scoreHand a
+
+scoreHand :: Hand -> Int
+scoreHand = sum . zipWith (*) [1 ..] . reverse
+
+-- | The hand that a player is holding.  First card in the list is the top card
+type Hand = [Int]
+
+-- | Who are the players
+data Player = Player1 | Player2 deriving (Show)
+
+-- | The state of a recursive game
+data RecState = RecState
+  { hands :: (Hand, Hand),
+    prevHands :: S.Set (Hand, Hand)
+  }
+  deriving (Show)
+
+-- | Who is the winner?
+recWinner :: RecState -> Maybe Player
+recWinner rs
+  | S.member (hands rs) (prevHands rs) = trace "REPEAT" (Just Player1)
+  | null (fst (hands rs)) = Just Player2
+  | null (snd (hands rs)) = Just Player1
+  | otherwise = Nothing
+
+-- | Play a recursive game
+recPlay :: RecState -> (Player, Hand)
+recPlay rs =
+  case recWinner rs of
+    Nothing -> recPlay (recNextState rs)
+    Just Player1 -> (Player1, fst (hands rs))
+    Just Player2 -> (Player2, snd (hands rs))
+
+-- | Next state in a recursive game
+recNextState :: RecState -> RecState
+recNextState rs@(RecState {hands = hs@(c1 : c1s, c2 : c2s), prevHands = phs}) =
+  case roundWinner of
+    Player1 -> rs {hands = (c1s ++ [c1, c2], c2s), prevHands = S.insert hs phs}
+    Player2 -> rs {hands = (c1s, c2s ++ [c2, c1]), prevHands = S.insert hs phs}
   where
-    score (a, b) = if null a then scoreHand b else scoreHand a
-    scoreHand = sum . zipWith (*) [1 ..] . reverse
+    -- Who wins this round?
+    roundWinner
+      | c1 <= length c1s && c2 <= length c2s = fst . recPlay $ subStart
+      | c1 < c2 = Player2
+      | c2 < c1 = Player1
+      | otherwise = error "cards should not tie"
+    -- Starting point for a recursive game
+    subStart = RecState {hands = (take c1 c1s, take c2 c2s), prevHands = S.empty}
+recNextState _ = error "should not have tried recNextState when there was a winner"
 
 part2 :: Problem -> Int
-part2 = length
+part2 problem =
+  scoreHand . snd . recPlay $ RecState {hands = problem, prevHands = S.empty}
