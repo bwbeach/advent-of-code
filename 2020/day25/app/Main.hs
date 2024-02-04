4f246d6c3ac1c625888b426eae35a7f1f549ce44
Brian Beach <coder@beachfamily.net>
2024-02-04 08:12:49 -1000

    2020 day 25 part 1

--- 2020/day25/app/Main.hs
+++ 2020/day25/app/Main.hs
@@ -1,17 +1,38 @@
 module Main where
 
 import Advent (run)
+import Data.List (elemIndex)
+import Data.Maybe (fromJust)
 
 main :: IO ()
 main = run parse part1 part2
 
-type Problem = String
+type Problem = (Int, Int)
 
 parse :: String -> Problem
-parse = id
+parse =
+  pairFromList . map read . words
+  where
+    pairFromList [a, b] = (a, b)
+    pairFromList as = error ("expected list of two: " ++ show as)
 
 part1 :: Problem -> Int
-part1 = length
+part1 (cardPublicKey, doorPublicKey) =
+  encryptionKey doorPublicKey cardLoopSize
+  where
+    cardLoopSize = loopSize 7 cardPublicKey
 
 part2 :: Problem -> Int
-part2 = length
+part2 _ = 0
+
+-- | The fundamental transformation that gets repeated
+transform :: Int -> Int -> Int
+transform subject value = (value * subject) `mod` 20201227
+
+-- | Find the loop size, given subject number and a public key.
+loopSize :: Int -> Int -> Int
+loopSize subject publicKey = fromJust . elemIndex publicKey . iterate (transform subject) $ 1
+
+-- | Find the encryption key, given a public key of one device and loop size of the other device
+encryptionKey :: Int -> Int -> Int
+encryptionKey onePublicKey otherLoopSize = (!! otherLoopSize) . iterate (transform onePublicKey) $ 1
