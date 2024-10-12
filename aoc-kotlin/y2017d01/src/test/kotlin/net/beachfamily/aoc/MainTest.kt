d9c2ab42bcb86085f7c541e039003daded966ea5
Brian Beach <coder@beachfamily.net>
2024-10-12 12:39:39 -1000

    y2017day01 part 2

--- aoc-kotlin/y2017d01/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
+++ aoc-kotlin/y2017d01/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
@@ -6,10 +6,19 @@ import kotlin.test.assertEquals
 class MainTest {
 
     @Test
-    fun `examples from problem statement`() {
+    fun `examples from part1 problem statement`() {
         assertEquals(3, part1("1122"))
         assertEquals(4, part1("1111"))
         assertEquals(0, part1("1234"))
         assertEquals(9, part1("91212129"))
+    }
+
+    @Test
+    fun `examples from part2 problem statement`() {
+        assertEquals(6, part2("1212"))
+        assertEquals(0, part2("1221"))
+        assertEquals(4, part2("123425"))
+        assertEquals(12, part2("123123"))
+        assertEquals(4, part2("12131415"))
     }
 }
