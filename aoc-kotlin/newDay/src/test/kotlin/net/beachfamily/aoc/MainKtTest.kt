5a578215b53b20b905624914f47e5d518a1fc3b7
Brian Beach <coder@beachfamily.net>
2024-10-19 10:46:58 -1000

    Add empty unit test.

---
+++ aoc-kotlin/newDay/src/test/kotlin/net/beachfamily/aoc/MainKtTest.kt
@@ -0,0 +1,12 @@
+package net.beachfamily.aoc
+
+import org.junit.jupiter.api.Assertions.*
+import kotlin.test.Test
+
+class MainKtTest {
+
+    @Test
+    fun testAddIncludeLineToProject() {
+        assertTrue(true)
+    }
+}
--- aoc-kotlin/newDay/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
+++ aoc-kotlin/newDay/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
@@ -2,4 +2,5 @@ package net.beachfamily.aoc
 
 class MainTest {
 
+
 }
