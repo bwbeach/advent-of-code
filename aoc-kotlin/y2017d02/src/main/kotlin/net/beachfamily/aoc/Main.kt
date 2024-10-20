34736adc15e2b478a225a15fb07a002368bf677b
Brian Beach <coder@beachfamily.net>
2024-10-19 14:07:30 -1000

    Template for y2017d02

---
+++ aoc-kotlin/y2017d02/src/main/kotlin/net/beachfamily/aoc/Main.kt
@@ -0,0 +1,17 @@
+package net.beachfamily.aoc
+
+private const val input = "the input"
+
+fun main() {
+    println(part1(input))
+    println(part2(input))
+}
+
+fun part1(s: String) : Int {
+    return s.length
+}
+
+fun part2(s: String) : Int {
+    return s.length
+}
+
