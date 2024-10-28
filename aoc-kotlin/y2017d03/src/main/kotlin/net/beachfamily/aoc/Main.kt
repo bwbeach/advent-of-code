614b6b5d60b9db973c03e0a6a3d359cd4a8c46b4
Brian Beach <coder@beachfamily.net>
2024-10-27 15:33:54 -1000

    y2017d03 part 1

--- aoc-kotlin/y2017d03/src/main/kotlin/net/beachfamily/aoc/Main.kt
+++ aoc-kotlin/y2017d03/src/main/kotlin/net/beachfamily/aoc/Main.kt
@@ -1,5 +1,7 @@
 package net.beachfamily.aoc
 
+import kotlin.math.abs
+
 fun main() {
     val input = readInput("y2017d03")
     val address = singleItem(words(input).asSequence()).toInt()
@@ -10,10 +12,91 @@ fun main() {
 }
 
 fun part1(a: Int) : Int {
-    return a
+    val (x, y) = xAndY(a)
+    return abs(x) + abs(y)
 }
 
 fun part2(s: String) : Int {
     return s.length
 }
 
+/**
+ * Returns the (x, y) coordinates of the given address.
+ */
+fun xAndY(a: Int) : Pair<Int, Int> {
+    if (a == 1) {
+        return Pair(0, 0)
+    } else {
+        val inner = sizeOfInner(a)
+        val first = inner * inner + 1
+        val edge = inner + 2
+        return Pair(x(a, first, edge), y(a, first, edge))
+    }
+}
+
+fun x(a: Int, first: Int, edge: Int) : Int {
+    // `n` is how many squares, counterclockwise, we are around the outer
+    // square, with 0 at the bottom right.
+    val n = a - first + 1
+    // `r` is the "radius" of the square.  The range of both x and
+    // y is [-r, r].
+    val r = edge / 2  // edge is odd; rounds down
+    // The number of new squares on each edge is the edge length minus one,
+    // because the corners count only once, not on both edges they participate
+    // in.
+    val edgeDelta = edge - 1
+
+    return when (n / edgeDelta) {
+        0 -> r
+        1 -> edgeDelta * 2 - n - r
+        2 -> -r
+        3 -> n - edgeDelta * 3 - r
+        4 -> r
+        else -> throw IllegalArgumentException("Invalid address $a when first=$first and edge=$edge")
+    }
+}
+
+fun y(a: Int, first: Int, edge: Int) : Int {
+    // `n` is how many squares, counterclockwise, we are around the outer
+    // square, with 0 at the bottom right.
+    val n = a - first + 1
+    // `r` is the "radius" of the square.  The range of both x and
+    // y is [-r, r].
+    val r = edge / 2  // edge is odd; rounds down
+    // The number of new squares on each edge is the edge length minus one,
+    // because the corners count only once, not on both edges they participate
+    // in.
+    val edgeDelta = edge - 1
+
+    return when (n / edgeDelta) {
+        0 -> n - r
+        1 -> r
+        2 -> edgeDelta * 3 - n - r
+        3 -> -r
+        4 -> -r
+        else -> throw IllegalArgumentException("Invalid address $a when first=$first and edge=$edge")
+    }
+}
+
+
+
+/**
+ * Returns the first number in the outer square, and the size of its edges.
+ */
+fun firstAndSize(a: Int) : Pair<Int, Int> {
+    assert(1 < a)
+    val inner = sizeOfInner(a)
+    val first = inner * inner + 1
+    return Pair(first, inner + 2)
+}
+
+/**
+ * Returns the largest odd n, such that n*n < a
+ */
+fun sizeOfInner(a: Int) : Int {
+    var n = 1
+    while ((n + 2) * (n + 2) < a) {
+        n += 2
+    }
+    return n
+}
--- aoc-kotlin/y2017d03/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
+++ aoc-kotlin/y2017d03/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
@@ -7,11 +7,38 @@ class MainTest {
 
     @Test
     fun `examples from part1 problem statement`() {
-        assertEquals(4, part1("1122"))
+        assertEquals(0, part1(1))
+        assertEquals(3, part1(12))
+        assertEquals(2, part1(23))
+        assertEquals(31, part1(1024))
     }
 
     @Test
     fun `examples from part2 problem statement`() {
         assertEquals(4, part2("1212"))
+    }
+
+    @Test
+    fun `test x function`() {
+        assertEquals(1, x(2, 2, 3))
+        assertEquals(1, x(3, 2, 3))
+        assertEquals(0, x(4, 2, 3))
+        assertEquals(-1, x(5, 2, 3))
+        assertEquals(-1, x(6, 2, 3))
+        assertEquals(-1, x(7, 2, 3))
+        assertEquals(0, x(8, 2, 3))
+        assertEquals(1, x(9, 2, 3))
+    }
+
+    @Test
+    fun `test y function`() {
+        assertEquals(0, y(2, 2, 3))
+        assertEquals(1, y(3, 2, 3))
+        assertEquals(1, y(4, 2, 3))
+        assertEquals(1, y(5, 2, 3))
+        assertEquals(0, y(6, 2, 3))
+        assertEquals(-1, y(7, 2, 3))
+        assertEquals(-1, y(8, 2, 3))
+        assertEquals(-1, y(9, 2, 3))
     }
 }
