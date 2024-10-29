2ca7b39e3fc3d3b006f66c5ff952d7cfd65566bc
Brian Beach <coder@beachfamily.net>
2024-10-28 16:24:40 -1000

    y2017d04 part 1

--- aoc-kotlin/y2017d04/src/main/kotlin/net/beachfamily/aoc/Main.kt
+++ aoc-kotlin/y2017d04/src/main/kotlin/net/beachfamily/aoc/Main.kt
@@ -1,16 +1,22 @@
 package net.beachfamily.aoc
 
 fun main() {
-    val input = readInput("y2017d01")
+    val input = readInput("y2017d04")
     println(part1(input))
     println(part2(input))
 }
 
 fun part1(s: String) : Int {
-    return s.length
+    return lines(s).filter { valid(it) }.size
 }
 
 fun part2(s: String) : Int {
     return s.length
+}
+
+fun valid(s: String) : Boolean {
+    val original = words(s)
+    val withoutDups = original.toSet()
+    return original.size == withoutDups.size
 }
 
--- aoc-kotlin/y2017d04/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
+++ aoc-kotlin/y2017d04/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
@@ -2,6 +2,8 @@ package net.beachfamily.aoc
 
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class MainTest {
 
@@ -13,5 +15,11 @@ class MainTest {
     @Test
     fun `examples from part2 problem statement`() {
         assertEquals(4, part2("1212"))
+    }
+
+    fun `test part1 valid check`() {
+        assertTrue { valid("aa bb cc dd ee") }
+        assertFalse { valid("aa bb cc dd aa") }
+        assertTrue { valid("aa bb cc dd aaa") }
     }
 }
