124b90fae711a65053f87d675b1169de4e8f0b6b
Brian Beach <coder@beachfamily.net>
2024-10-30 17:21:08 -1000

    y2017d06 part 1

--- aoc-kotlin/y2017d06/src/main/kotlin/net/beachfamily/aoc/Main.kt
+++ aoc-kotlin/y2017d06/src/main/kotlin/net/beachfamily/aoc/Main.kt
@@ -1,16 +1,60 @@
 package net.beachfamily.aoc
 
 fun main() {
-    val input = readInput("y2017d01")
-    println(part1(input))
+    val input = readInput("y2017d06")
+    val bins = words(input).map { it.toInt() }
+    println(part1(bins))
     println(part2(input))
 }
 
-fun part1(s: String) : Int {
-    return s.length
+fun part1(start : List<Int>) : Int {
+    return indexOfFirstRepeated(generateSequence(start) { step(it) }) ?: -1
 }
 
 fun part2(s: String) : Int {
     return s.length
 }
 
+fun step(bins: List<Int>) : List<Int> {
+    val i = indexOfBiggest(bins.asSequence())
+    val binSize = bins[i]
+    
+    val result = bins.toMutableList()
+    result[i] = 0
+    for (j in 1..binSize) {
+        result[(i + j) % bins.size] += 1
+    }
+
+    return result.toList()
+}
+
+fun indexOfBiggest(bins: Sequence<Int>) : Int {
+    return bins.withIndex().maxWith(::compareIndexedValues).index ;
+}
+
+/**
+ * Returns the index of the first repeated item in the Sequence.
+ */
+fun <T> indexOfFirstRepeated(sequence: Sequence<T>): Int? {
+    val seen = mutableSetOf<T>()
+    sequence.forEachIndexed { index, item ->
+        if (!seen.add(item)) return index
+    }
+    return null
+}
+
+fun compareIndexedValues(pair1: IndexedValue<Int>, pair2: IndexedValue<Int>): Int {
+    val firstComparison = pair1.value.compareTo(pair2.value)
+    return if (firstComparison != 0) firstComparison else - pair1.index.compareTo(pair2.index)
+}
+
+/**
+ * Compares two Pair objects whose elements are comparable.
+ *
+ * The primary comparison is on the first element, falling back to the
+ * second element when the first elements are equal.
+ */
+fun <A : Comparable<A>, B : Comparable<B>> comparePairs(pair1: Pair<A, B>, pair2: Pair<A, B>): Int {
+    val firstComparison = pair1.first.compareTo(pair2.first)
+    return if (firstComparison != 0) firstComparison else pair1.second.compareTo(pair2.second)
+}
--- aoc-kotlin/y2017d06/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
+++ aoc-kotlin/y2017d06/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
@@ -2,16 +2,47 @@ package net.beachfamily.aoc
 
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNull
 
 class MainTest {
 
     @Test
     fun `examples from part1 problem statement`() {
-        assertEquals(4, part1("1122"))
+        assertEquals(5, part1(listOf(0, 2, 7, 0)))
     }
 
     @Test
     fun `examples from part2 problem statement`() {
         assertEquals(4, part2("1212"))
+    }
+
+    @Test
+    fun `test indexOfBiggest`() {
+        assertEquals(2, indexOfBiggest(sequenceOf(0, 2, 7, 0)))
+        assertEquals(0, indexOfBiggest(sequenceOf(3, 1, 2, 3)))
+    }
+
+    fun `test indexOfFirstRepeated`() {
+        assertNull(indexOfFirstRepeated(sequenceOf(0, 2, 7)))
+        assertEquals(3, indexOfFirstRepeated(sequenceOf(3, 1, 2, 3)))
+    }
+
+    @Test
+    fun `test step by step`() {
+        
+        val start = listOf(0, 2, 7, 0)
+        val firstSix = generateSequence(start) { step(it) }.take(6).toList()
+
+        assertEquals(
+            listOf(
+                listOf(0, 2, 7, 0),
+                listOf(2, 4, 1, 2),
+                listOf(3, 1, 2, 3),
+                listOf(0, 2, 3, 4),
+                listOf(1, 3, 4, 1),
+                listOf(2, 4, 1, 2)
+            ),
+            firstSix
+        )
     }
 }
