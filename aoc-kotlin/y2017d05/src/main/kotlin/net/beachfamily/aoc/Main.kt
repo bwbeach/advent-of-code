9f84c4bcb7fad8fbf40f25bdd0e48a33d60012dd
Brian Beach <coder@beachfamily.net>
2024-10-30 15:18:42 -1000

    y2017d05 part 1

--- aoc-kotlin/y2017d05/src/main/kotlin/net/beachfamily/aoc/Main.kt
+++ aoc-kotlin/y2017d05/src/main/kotlin/net/beachfamily/aoc/Main.kt
@@ -1,16 +1,41 @@
 package net.beachfamily.aoc
 
 fun main() {
-    val input = readInput("y2017d01")
-    println(part1(input))
+    val input = readInput("y2017d05")
+    println(part1(parse(input)))
     println(part2(input))
 }
 
-fun part1(s: String) : Int {
-    return s.length
+fun parse(s: String) : Program {
+    val instructions = words(s).map{  it.toInt() }.toMutableList()
+    return Program(instructions, 0)
+} 
+
+fun part1(p: Program) : Int {
+    while (!p.isDone()) {
+        p.step()
+    }
+    return p.count
 }
 
 fun part2(s: String) : Int {
     return s.length
+}
+
+
+data class Program(
+    val instructions: MutableList<Int>,
+    var pc: Int,
+    var count: Int = 0
+) {
+    fun step() {
+        val delta = instructions[pc]
+        instructions[pc] = delta + 1
+        pc += delta
+        count += 1
+    }
+    
+    fun isDone() : Boolean = 
+        pc < 0 || pc >= instructions.size
 }
 
--- aoc-kotlin/y2017d05/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
+++ aoc-kotlin/y2017d05/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
@@ -2,12 +2,26 @@ package net.beachfamily.aoc
 
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class MainTest {
 
     @Test
     fun `examples from part1 problem statement`() {
-        assertEquals(4, part1("1122"))
+        val p = Program(mutableListOf(0, 3, 0, 1, -3), 0)
+        p.step()
+        assertEquals(Program(mutableListOf(1, 3, 0, 1, -3), 0, 1), p)
+        p.step()
+        assertEquals(Program(mutableListOf(2, 3, 0, 1, -3), 1, 2), p)
+        p.step()
+        assertEquals(Program(mutableListOf(2, 4, 0, 1, -3), 4, 3), p)
+        p.step()
+        assertEquals(Program(mutableListOf(2, 4, 0, 1, -2), 1, 4), p)
+        assertFalse(p.isDone())
+        p.step()
+        assertEquals(Program(mutableListOf(2, 5, 0, 1, -2), 5, 5), p)
+        assertTrue(p.isDone())
     }
 
     @Test
