301a28f84d6de5dee3c61faabbeef9a82feae6b8
Brian Beach <coder@beachfamily.net>
2024-11-13 18:11:11 -0800

    y2017d09 part1

--- aoc-kotlin/y2017d09/src/main/kotlin/net/beachfamily/aoc/Main.kt
+++ aoc-kotlin/y2017d09/src/main/kotlin/net/beachfamily/aoc/Main.kt
@@ -1,16 +1,128 @@
 package net.beachfamily.aoc
 
+sealed class Item {
+    data class Garbage(val text: String) : Item()
+    data class Group(val items: List<Item>) : Item()
+}
+
+/**
+ * Parses one item of garbage.  Assumes leading '<' has already been consumed.
+ */
+fun parseGarbage(input: Iterator<Char>): Item {
+    val result = StringBuilder()
+    while (true) {
+        val c = input.next()
+        if (c == '!') {
+            input.next()
+        } else if (c == '>') {
+            return Item.Garbage(result.toString())
+        } else {
+            result.append(c)
+        }
+    }
+}
+
+/**
+ * Parse one group.  Assumes the leading '{' has already been consumed.
+ */
+fun parseGroup(input: Iterator<Char>): Item {
+    val items = mutableListOf<Item>()
+    while (true) {
+        val c = input.next()
+        if (c == '}') {
+            return Item.Group(items)
+        } else if (c == '<') {
+            items.add(parseGarbage(input))
+        } else if (c == '{') {
+            items.add(parseGroup(input))
+        }
+        val t = input.next()
+        if (t == '}') {
+            return Item.Group(items)
+        } else {
+            if (t != ',') {
+                println("AHA!")
+            }
+            require(t == ',')
+        }
+    }
+}
+
+fun countGroups(item: Item): Int {
+    when (item) {
+        is Item.Garbage -> return 0
+        is Item.Group -> return 1 + item.items.map { countGroups(it) }.sum()
+    }
+}
+
+fun score(item: Item, depth: Int = 1): Int {
+    when (item) {
+        is Item.Garbage -> return 0
+        is Item.Group -> return depth + item.items.map { score(it, depth + 1) }.sum()
+    }
+}
+
 fun main() {
-    val input = readInput("y2017d01")
+    val input = readInput("y2017d09").trim()
     println(part1(input))
+    println(part1b(input))
     println(part2(input))
 }
 
 fun part1(s: String) : Int {
-    return s.length
+    val iter = s.iterator()
+    require(iter.next() == '{')
+    val item = parseGroup(iter)
+    return score(item)
 }
 
 fun part2(s: String) : Int {
     return s.length
 }
 
+enum class State {
+    GROUP,
+    GARBAGE,
+    DELETE,
+}
+
+data class FullState(
+    val state: State,
+    val depth: Int,
+    val score: Int
+) {
+    fun next(c: Char): FullState {
+        return when (state) {
+            State.GROUP ->
+                when (c) {
+                    '{' -> FullState(State.GROUP, depth + 1, score + depth + 1)
+                    '}' -> {
+                        require(0 < depth)
+                        FullState(State.GROUP, depth - 1, score)
+                    }
+                    ',' -> this
+                    '<' -> FullState(State.GARBAGE, depth, score)
+                    else -> throw IllegalArgumentException("Invalid char '$c' in state $this")
+                }
+            State.GARBAGE -> {
+                when (c) {
+                    '!' -> FullState(State.DELETE, depth, score)
+                    '>' -> FullState(State.GROUP, depth, score)
+                    else -> this
+                }
+            }
+            State.DELETE ->
+                FullState(State.GARBAGE, depth, score)
+        }
+    }
+}
+
+fun initialState(): FullState = FullState(State.GROUP, 0, 0)
+
+fun part1b(s: String): Int {
+    var state = initialState()
+    for (c in s) {
+        state = state.next(c)
+    }
+    return state.score
+}
--- aoc-kotlin/y2017d09/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
+++ aoc-kotlin/y2017d09/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
@@ -2,16 +2,77 @@ package net.beachfamily.aoc
 
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 
 class MainTest {
 
     @Test
-    fun `examples from part1 problem statement`() {
-        assertEquals(4, part1("1122"))
+    fun `examples from part2 problem statement`() {
+        assertEquals(4, part2("1212"))
+    }
+
+    val garbageExamples = listOf(
+        "<>" to "",
+        "<random characters>" to "random characters",
+        "<<<<>" to "<<<",
+        "<{!>}>" to "{}",
+        "<!!>" to "",
+        "<!!!>>" to "",
+        "<{o\"i!a,<{i<a>" to "{o\"i,<{i<a",
+    )
+
+    @Test
+    fun `test parseGarbage`() {
+        for ((text, expected) in garbageExamples) {
+            val iterator = text.iterator()
+            assertEquals('<', iterator.next())
+            val actual = parseGarbage(iterator)
+            assertEquals(Item.Garbage(expected), actual)
+            assertFalse(iterator.hasNext())
+        }
+    }
+
+    val groupCountExamples = listOf(
+        "{}" to 1,
+        "{{{}}}" to 3,
+        "{{},{}}" to 3,
+        "{{{},{},{{}}}}" to 6,
+        "{<{},{},{{}}>}" to 1,
+        "{<a>,<a>,<a>,<a>}" to 1,
+        "{{<a>},{<a>},{<a>},{<a>}}" to 5,
+        "{{<!>},{<!>},{<!>},{<a>}}" to 2,
+    )
+
+    @Test
+    fun `test parseGroup`() {
+        for ((text, expected) in groupCountExamples) {
+            val iterator = text.iterator()
+            assertEquals('{', iterator.next())
+            val group = parseGroup(iterator)
+            assertEquals(expected, countGroups(group))
+            assertFalse(iterator.hasNext())
+        }
+    }
+
+    val scoreExamples = listOf(
+        "{}" to 1,
+        "{{{}}}" to 6,
+        "{{},{}}" to 5,
+        "{<a>,<a>,<a>,<a>}" to 1,
+        "{{<a!>},{<a!>},{<a!>},{<ab>}}" to 3,
+    )
+
+    @Test
+    fun `testScore`() {
+        for ((text, expected) in scoreExamples) {
+            assertEquals(expected, part1(text))
+        }
     }
 
     @Test
-    fun `examples from part2 problem statement`() {
-        assertEquals(4, part2("1212"))
+    fun `test part1b`() {
+        for ((text, expected) in scoreExamples) {
+            assertEquals(expected, part1b(text))
+        }
     }
 }
