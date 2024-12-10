6f261de859997d9fc3fdc3dea86e696512f89ea1
Brian Beach <coder@beachfamily.net>
2024-12-09 14:07:55 -1000

    y2024d03 part 2

--- aoc-kotlin/y2024d03/src/main/kotlin/net/beachfamily/aoc/Main.kt
+++ aoc-kotlin/y2024d03/src/main/kotlin/net/beachfamily/aoc/Main.kt
@@ -24,8 +24,32 @@ fun part1(s: String) : Int {
     return findMuls(s).map { it.first * it.second }.sum()
 }
 
+fun findMuls2(s: String): List<Pair<Int, Int>> {
+    val pattern = Pattern.compile("""mul\((\d+),(\d+)\)|do\(\)|don't\(\)""")
+    val matcher = pattern.matcher(s)
+    val result = mutableListOf<Pair<Int, Int>>()
+    var enabled = true
+    while (matcher.find()) {
+        val whole = matcher.group()
+        if (whole == "do()") {
+            enabled = true
+        } else if (whole == "don't()") {
+            enabled = false
+        } else if (whole.startsWith("mul(")) {
+            val first = matcher.group(1).toInt()
+            val second = matcher.group(2).toInt()
+            if (enabled) {
+                result.add(Pair(first, second))
+            }
+        } else {
+            throw IllegalArgumentException("Unexpected: $whole")
+        }
+    }
+    return result
+}
+
 fun part2(s: String): Int {
-    return s.length
+    return findMuls2(s).map { it.first * it.second }.sum()
 }
 
 
--- aoc-kotlin/y2024d03/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
+++ aoc-kotlin/y2024d03/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
@@ -22,4 +22,12 @@ class MainTest {
             findMuls("""xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))""")
         )
     }
+
+    @Test
+    fun testFindMuls2() {
+        assertEquals(
+            listOf(2 to 4, 8 to 5),
+            findMuls2("""xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))""")
+        )
+    }
 }
