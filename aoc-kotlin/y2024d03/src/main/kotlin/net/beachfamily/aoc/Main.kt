21b12e17b4203d2f7482babaf47619a82ecc8e62
Brian Beach <coder@beachfamily.net>
2024-12-09 14:15:12 -1000

    switch from List to Sequence

--- aoc-kotlin/y2024d03/src/main/kotlin/net/beachfamily/aoc/Main.kt
+++ aoc-kotlin/y2024d03/src/main/kotlin/net/beachfamily/aoc/Main.kt
@@ -8,44 +8,44 @@ fun main() {
     println(part2(input))
 }
 
-fun findMuls(s: String): List<Pair<Int, Int>> {
+fun findMuls(s: String): Sequence<Pair<Int, Int>> {
     val pattern = Pattern.compile("""mul\((\d+),(\d+)\)""")
     val matcher = pattern.matcher(s)
-    val result = mutableListOf<Pair<Int, Int>>()
-    while (matcher.find()) {
-        val first = matcher.group(1).toInt()
-        val second = matcher.group(2).toInt()
-        result.add(Pair(first, second))
+    return sequence {
+        while (matcher.find()) {
+            val first = matcher.group(1).toInt()
+            val second = matcher.group(2).toInt()
+            yield(Pair(first, second))
+        }
     }
-    return result
 }
 
 fun part1(s: String) : Int {
     return findMuls(s).map { it.first * it.second }.sum()
 }
 
-fun findMuls2(s: String): List<Pair<Int, Int>> {
+fun findMuls2(s: String): Sequence<Pair<Int, Int>> {
     val pattern = Pattern.compile("""mul\((\d+),(\d+)\)|do\(\)|don't\(\)""")
     val matcher = pattern.matcher(s)
-    val result = mutableListOf<Pair<Int, Int>>()
     var enabled = true
-    while (matcher.find()) {
-        val whole = matcher.group()
-        if (whole == "do()") {
-            enabled = true
-        } else if (whole == "don't()") {
-            enabled = false
-        } else if (whole.startsWith("mul(")) {
-            val first = matcher.group(1).toInt()
-            val second = matcher.group(2).toInt()
-            if (enabled) {
-                result.add(Pair(first, second))
+    return sequence {
+        while (matcher.find()) {
+            val whole = matcher.group()
+            if (whole == "do()") {
+                enabled = true
+            } else if (whole == "don't()") {
+                enabled = false
+            } else if (whole.startsWith("mul(")) {
+                val first = matcher.group(1).toInt()
+                val second = matcher.group(2).toInt()
+                if (enabled) {
+                    yield(Pair(first, second))
+                }
+            } else {
+                throw IllegalArgumentException("Unexpected: $whole")
             }
-        } else {
-            throw IllegalArgumentException("Unexpected: $whole")
         }
     }
-    return result
 }
 
 fun part2(s: String): Int {
