e71062d3c8ca09e290d66c807250a537315f9bef
Brian Beach <coder@beachfamily.net>
2024-12-09 14:13:19 -1000

    y2024d04 skeleton

---
+++ aoc-kotlin/y2024d04/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    application
+    kotlin("jvm") version "2.0.20"
+}
+
+application {
+    mainClass.set("net.beachfamily.aoc.MainKt")
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation(project(":aocLib"))
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
