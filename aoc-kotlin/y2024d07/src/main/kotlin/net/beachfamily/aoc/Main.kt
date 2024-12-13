f91e07952dd5c132f80ee41c5848df3f41862cc5
Brian Beach <coder@beachfamily.net>
2024-12-13 12:08:25 -1000

    y2024d07 part 1

--- aoc-kotlin/y2024d07/src/main/kotlin/net/beachfamily/aoc/Main.kt
+++ aoc-kotlin/y2024d07/src/main/kotlin/net/beachfamily/aoc/Main.kt
@@ -11,9 +11,11 @@ fun main() {
     println(part2(input))
 }
 
-fun part1(s: String) : Int {
-    return s.length
-}
+fun part1(s: String) : Long =
+    parse(s)
+        .filter(::equationWorks)
+        .map { it.testValue }
+        .sum()
 
 fun part2(s: String) : Int {
     return s.length
@@ -29,3 +31,28 @@ fun parseEquation(s: String): Equation {
         words(b).map { it.toLong() }.asSequence()
     )
 }
+
+fun equationWorks(e: Equation): Boolean {
+    return allValues(e.numbers).contains(e.testValue)
+}
+
+fun allValues(numbers: Sequence<Long>): Sequence<Long> {
+    val (head, tail) = numbers.headAndTail()
+    val ops = sequenceOf<(Long, Long) -> Long>(
+        { a, b -> a + b },
+        { a, b -> a * b },
+    )
+    return tail.fold(sequenceOf(head), { a, b -> tryAllOps(a, ops, b) })
+}
+
+/**
+ * Returns a sequence of the results of combining each of the
+ * values in the LHS input sequence with the value on the RHS
+ * using each of the provided operators.
+ */
+fun <T> tryAllOps(
+    lhs: Sequence<T>,
+    ops: Sequence<(T, T) -> T>,
+    rhs: T
+) =
+    lhs.flatMap { lhsVal -> ops.map { op -> op(lhsVal, rhs) } }
--- aoc-kotlin/y2024d07/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
+++ aoc-kotlin/y2024d07/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
@@ -5,9 +5,22 @@ import kotlin.test.assertEquals
 
 class MainTest {
 
+    fun testInput() =
+        """
+        190: 10 19
+        3267: 81 40 27
+        83: 17 5
+        156: 15 6
+        7290: 6 8 6 15
+        161011: 16 10 13
+        192: 17 8 14
+        21037: 9 7 18 13
+        292: 11 6 16 20    
+        """.trimIndent()
+
     @Test
     fun `examples from part1 problem statement`() {
-        assertEquals(4, part1("1122"))
+        assertEquals(3749, part1(testInput()))
     }
 
     @Test
