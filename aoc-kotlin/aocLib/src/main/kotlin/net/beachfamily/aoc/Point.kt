268c472d14d1ea40373256ca9c2ae2dd5eb1d8ec
Brian Beach <coder@beachfamily.net>
2024-12-14 08:32:30 -1000

    Add more methods to Point

--- aoc-kotlin/aocLib/src/main/kotlin/net/beachfamily/aoc/Point.kt
+++ aoc-kotlin/aocLib/src/main/kotlin/net/beachfamily/aoc/Point.kt
@@ -7,4 +7,12 @@ data class Point(val x: Int, val y: Int) {
     operator fun plus(other: Point): Point {
         return Point(x + other.x, y + other.y)
     }
+
+    operator fun minus(other: Point): Point {
+        return Point(x - other.x, y - other.y)
+    }
+
+    operator fun unaryMinus(): Point {
+        return Point(-x, -y)
+    }
 }
---
+++ aoc-kotlin/aocLib/src/test/kotlin/net/beachfamily/aoc/PointTest.kt
@@ -0,0 +1,32 @@
+package net.beachfamily.aoc
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class PointTest {
+
+    @Test
+    fun plus() {
+        assertEquals(
+            Point(5, 10),
+            Point(1, 2) + Point(4, 8)
+        )
+    }
+
+    @Test
+    fun minus() {
+        assertEquals(
+            Point(11, 7),
+            Point(15, 15) - Point(4, 8)
+        )
+    }
+
+    @Test
+    fun unaryMinus() {
+        assertEquals(
+            Point(-1, -2),
+            -Point(1, 2)
+        )
+    }
+}
