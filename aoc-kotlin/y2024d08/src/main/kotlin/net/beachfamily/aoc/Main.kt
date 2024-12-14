013b3432da91e1ecf11bf752da9cea2dc26bb03c
Brian Beach <coder@beachfamily.net>
2024-12-13 20:57:29 -1000

    y2024d08 skeleton

---
+++ aoc-kotlin/y2024d08/src/main/kotlin/net/beachfamily/aoc/Main.kt
@@ -0,0 +1,16 @@
+package net.beachfamily.aoc
+
+fun main() {
+    val input = readInput("y2024d08")
+    println(part1(input))
+    println(part2(input))
+}
+
+fun part1(s: String) : Int {
+    return s.length
+}
+
+fun part2(s: String) : Int {
+    return s.length
+}
+
