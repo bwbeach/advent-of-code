727904e46b20416a64dd3e9fd1f30451639d8e0f
Brian Beach <coder@beachfamily.net>
2024-12-17 12:50:41 -1000

    y2024d10 part 1

--- aoc-kotlin/y2024d10/src/main/kotlin/net/beachfamily/aoc/Main.kt
+++ aoc-kotlin/y2024d10/src/main/kotlin/net/beachfamily/aoc/Main.kt
@@ -1,5 +1,7 @@
 package net.beachfamily.aoc
 
+import com.google.common.collect.Multimap
+
 fun main() {
     val input = readInput("y2024d10")
     println(part1(input))
@@ -7,7 +9,40 @@ fun main() {
 }
 
 fun part1(s: String) : Int {
-    return s.length
+    val grid = gridOfInt(s)
+    val posToPeaks = reachablePeaks(grid, 0)
+    return posToPeaks.entries().asSequence().count()
+}
+
+fun reachablePeaks(grid: Grid<Int>, fromHeight: Int): Multimap<Point, Point> {
+    val pointsAtHeight =
+        grid.data
+            .entries
+            .asSequence()
+            .filter { (k, v) -> v == fromHeight }
+    return if (fromHeight == 9) {
+        pointsAtHeight
+            .map { (k, v) -> Pair(k, k) }
+            .toSetMultimap()
+    } else {
+        val nextHigher = reachablePeaks(grid, fromHeight + 1)
+        pointsAtHeight
+            .flatMap { (k, v) ->
+                neighbors(k)
+                    .filter { grid.data.containsKey(it) && grid.data[it] == fromHeight + 1 }
+                    .flatMap { nextHigher.get(it).asSequence().map { Pair(k, it!!)} }
+            }
+            .toSetMultimap()
+    }
+}
+
+fun neighbors(p: Point): Sequence<Point> {
+    return sequenceOf(
+        Point(p.x - 1, p.y),
+        Point(p.x + 1, p.y),
+        Point(p.x, p.y - 1),
+        Point(p.x, p.y + 1),
+    )
 }
 
 fun part2(s: String) : Int {
--- aoc-kotlin/y2024d10/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
+++ aoc-kotlin/y2024d10/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
@@ -5,9 +5,21 @@ import kotlin.test.assertEquals
 
 class MainTest {
 
+    fun testInput() =
+        """
+        89010123
+        78121874
+        87430965
+        96549874
+        45678903
+        32019012
+        01329801
+        10456732
+        """.trimIndent()
+
     @Test
     fun `examples from part1 problem statement`() {
-        assertEquals(4, part1("1122"))
+        assertEquals(36, part1(testInput()))
     }
 
     @Test
