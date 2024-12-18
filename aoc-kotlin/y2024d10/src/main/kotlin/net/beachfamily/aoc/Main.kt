21e5cb9bad793cb770a0e1232c5046217fa50712
Brian Beach <coder@beachfamily.net>
2024-12-17 21:31:46 -1000

    refactor part 1 to be structured like part 2

--- aoc-kotlin/y2024d10/src/main/kotlin/net/beachfamily/aoc/Main.kt
+++ aoc-kotlin/y2024d10/src/main/kotlin/net/beachfamily/aoc/Main.kt
@@ -11,13 +11,19 @@ fun main() {
 fun part1(s: String) : Int {
     val grid = gridOfInt(s)
     val posToPeaks = reachablePeaks(grid, 0)
-    return posToPeaks.entries().asSequence().count()
+    return posToPeaks.values.map { it.size }.sum()
+}
+
+fun part2(s: String) : Int {
+    val grid = gridOfInt(s)
+    val posToPeaks = pathCount(grid, 0)
+    return posToPeaks.values.sum()
 }
 
 /**
  * Returns a Multimap from a point at a given height to all the peaks reachable from it.
  */
-fun reachablePeaks(grid: Grid<Int>, fromHeight: Int): Multimap<Point, Point> {
+fun reachablePeaks(grid: Grid<Int>, fromHeight: Int): Map<Point, Set<Point>> {
     val pointsAtHeight =
         grid.data
             .entries
@@ -26,18 +32,14 @@ fun reachablePeaks(grid: Grid<Int>, fromHeight: Int): Multimap<Point, Point> {
             .map { it.key }
 
     return if (fromHeight == 9) {
-        pointsAtHeight
-            .map { k -> Pair(k, k) }
-            .toSetMultimap()
+        pointsAtHeight.associateWith { setOf(it) }
     } else {
         val nextHigher = reachablePeaks(grid, fromHeight + 1)
-        pointsAtHeight
-            .flatMap { k ->
-                neighbors(k)
-                    .filter { grid.data.containsKey(it) && grid.data[it] == fromHeight + 1 }
-                    .flatMap { nextHigher.get(it).asSequence().map { Pair(k, it!!)} }
-            }
-            .toSetMultimap()
+        pointsAtHeight.associateWith {
+            neighbors(it)
+                .mapNotNull { nextHigher[it] }
+                .fold(setOf<Point>()) { acc, set -> acc.union(set) }
+        }
     }
 }
 
@@ -71,11 +73,5 @@ fun neighbors(p: Point): Sequence<Point> {
         Point(p.x, p.y - 1),
         Point(p.x, p.y + 1),
     )
-}
-
-fun part2(s: String) : Int {
-    val grid = gridOfInt(s)
-    val posToPeaks = pathCount(grid, 0)
-    return posToPeaks.values.sum()
 }
 
