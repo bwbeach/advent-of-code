fbc787b8f3b6c180cce01bbfe1a283cc5e9ba352
Brian Beach <coder@beachfamily.net>
2024-12-18 14:16:02 -1000

    y2024d11 part 1

--- aoc-kotlin/y2024d11/src/main/kotlin/net/beachfamily/aoc/Main.kt
+++ aoc-kotlin/y2024d11/src/main/kotlin/net/beachfamily/aoc/Main.kt
@@ -7,10 +7,31 @@ fun main() {
 }
 
 fun part1(s: String) : Int {
-    return s.length
+    val input = words(s).map { it.toLong() }.asSequence()
+    val repeated = generateSequence(input) { blink(it)  }
+    return repeated.drop(25).first().count()
 }
 
 fun part2(s: String) : Int {
     return s.length
 }
+
+fun blink(stones: Sequence<Long>): Sequence<Long> = stones.flatMap { blinkOne(it) }
+
+fun blinkOne(stone: Long): Sequence<Long> =
+    sequence {
+        if (stone == 0L) {
+            yield(1)
+        } else {
+            val digits = stone.toString()
+            val len = digits.length
+            if (len % 2 == 0) {
+                yield(digits.substring(0, len / 2).toLong())
+                yield(digits.substring(len / 2).toLong())
+            } else {
+                require(stone < Long.MAX_VALUE / 2024)
+                yield(stone * 2024)
+            }
+        }
+    }
 
--- aoc-kotlin/y2024d11/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
+++ aoc-kotlin/y2024d11/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
@@ -5,13 +5,22 @@ import kotlin.test.assertEquals
 
 class MainTest {
 
+    fun testInput() = "125 17"
+
     @Test
     fun `examples from part1 problem statement`() {
-        assertEquals(4, part1("1122"))
+        assertEquals(55312, part1(testInput()))
     }
 
     @Test
     fun `examples from part2 problem statement`() {
         assertEquals(4, part2("1212"))
+    }
+
+    @Test
+    fun testBlinkOne() {
+        assertEquals(listOf(1), blinkOne(0).toList())
+        assertEquals(listOf(10, 0), blinkOne(1000).toList())
+        assertEquals(listOf(2024), blinkOne(1).toList())
     }
 }
