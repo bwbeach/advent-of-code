707a573e9d82e754abc09ff1f7427a99c66fe281
Brian Beach <coder@beachfamily.net>
2024-12-19 13:02:41 -1000

    y2024d13 part 1

--- aoc-kotlin/y2024d13/src/main/kotlin/net/beachfamily/aoc/Main.kt
+++ aoc-kotlin/y2024d13/src/main/kotlin/net/beachfamily/aoc/Main.kt
@@ -7,11 +7,52 @@ fun main() {
 }
 
 fun part1(s: String) : Int {
-    return s.length
+    return parseProblems(s)
+        .mapNotNull { it.solve() }
+        .map { (a, b) -> a * 3 + b }
+        .sum()
+}
+
+fun parseProblems(s : String): Sequence<Problem> {
+    return lines(s)
+        .asSequence()
+        .splitBy { it.isBlank() }
+        .map { parseProblemInput(it.joinToString("\n")) }
+}
+
+fun parseProblemInput(input: String): Problem {
+    val regex = """Button A: X\+(\d+), Y\+(\d+)\nButton B: X\+(\d+), Y\+(\d+)\nPrize: X=(\d+), Y=(\d+)""".toRegex()
+    val matchResult = regex.find(input) ?: throw IllegalArgumentException("Input format does not match the expected pattern.")
+
+    val (ax, ay, bx, by, px, py) = matchResult.destructured
+    return Problem(ax.toInt(), ay.toInt(), bx.toInt(), by.toInt(), px.toInt(), py.toInt())
 }
 
 fun part2(s: String) : Int {
     return s.length
+}
+
+data class Problem(
+    val ax: Int,
+    val ay: Int,
+    val bx: Int,
+    val by: Int,
+    val px: Int,
+    val py: Int,
+) {
+    fun solve(): Pair<Int, Int>? {
+        println("$this")
+        val det = (ax * by) - (ay * bx)
+        require(det != 0)
+        val detA = (px * by) - (py * bx)
+        val detB = (ax * py) - (ay * px)
+        println("$det $detA $detB ${detA % det} ${detB % det}")
+        if (detA % det != 0 || detB % det != 0) {
+            return null
+        } else {
+            return Pair(detA / det, detB / det)
+        }
+    }
 }
 
 
--- aoc-kotlin/y2024d13/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
+++ aoc-kotlin/y2024d13/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
@@ -5,9 +5,28 @@ import kotlin.test.assertEquals
 
 class MainTest {
 
+    fun testInput() =
+        """
+        Button A: X+94, Y+34
+        Button B: X+22, Y+67
+        Prize: X=8400, Y=5400
+
+        Button A: X+26, Y+66
+        Button B: X+67, Y+21
+        Prize: X=12748, Y=12176
+
+        Button A: X+17, Y+86
+        Button B: X+84, Y+37
+        Prize: X=7870, Y=6450
+
+        Button A: X+69, Y+23
+        Button B: X+27, Y+71
+        Prize: X=18641, Y=10279
+        """.trimIndent()
+
     @Test
     fun `examples from part1 problem statement`() {
-        assertEquals(4, part1("1122"))
+        assertEquals(480, part1(testInput()))
     }
 
     @Test
