19ea435d5541daf3dfc88212099f3e20765c9f1e
Brian Beach <coder@beachfamily.net>
2024-12-19 12:17:01 -1000

    y2024d13 skeleton

---
+++ aoc-kotlin/y2024d13/src/main/kotlin/net/beachfamily/aoc/Main.kt
@@ -0,0 +1,18 @@
+package net.beachfamily.aoc
+
+fun main() {
+    val input = readInput("y2024d13")
+    println(part1(input))
+    println(part2(input))
+}
+
+fun part1(s: String) : Int {
+    return s.length
+}
+
+fun part2(s: String) : Int {
+    return s.length
+}
+
+
+
---
+++ aoc-kotlin/y2024d13/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
@@ -0,0 +1,17 @@
+package net.beachfamily.aoc
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class MainTest {
+
+    @Test
+    fun `examples from part1 problem statement`() {
+        assertEquals(4, part1("1122"))
+    }
+
+    @Test
+    fun `examples from part2 problem statement`() {
+        assertEquals(4, part2("1212"))
+    }
+}
