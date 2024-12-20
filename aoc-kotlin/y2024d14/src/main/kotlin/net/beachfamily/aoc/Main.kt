d7bbca501f791ed719d768ceb0cff5ddc45b208b
Brian Beach <coder@beachfamily.net>
2024-12-20 07:16:05 -1000

    y2024d14 part 2

--- aoc-kotlin/y2024d14/src/main/kotlin/net/beachfamily/aoc/Main.kt
+++ aoc-kotlin/y2024d14/src/main/kotlin/net/beachfamily/aoc/Main.kt
@@ -1,9 +1,13 @@
 package net.beachfamily.aoc
 
+import java.awt.Color
+import javax.swing.JFrame
+import javax.swing.JPanel
+
 fun main() {
     val input = readInput("y2024d14")
     println(part1(input, 101, 103))
-    println(part2(input))
+    println(part2(input, 101, 103))
 }
 
 fun part1(s: String, width: Int, height: Int) : Int {
@@ -17,8 +21,30 @@ fun part1(s: String, width: Int, height: Int) : Int {
 
 fun Iterable<Int>.product(): Int = fold(1) { acc, i -> acc * i }
 
-fun part2(s: String) : Int {
-    return s.length
+fun part2(s: String, width: Int, height: Int) : Int {
+    val robots = lines(s).map(::parseRobot).asSequence()
+    val display = Display.Factory.create(width, height)
+    var bestSoFar = 0
+    for (i in generateSequence(0) { it + 1 }) {
+        val positions = robots.map { it.posAfter(i, width, height) }
+        val nInside = positions.filter { insideTree(it) }.count()
+        if (nInside > bestSoFar) {
+            bestSoFar = nInside
+            println("i = $i   count = $bestSoFar")
+            display.update(positions.toSet())
+            Thread.sleep(1000)
+        }
+    }
+    return 0
+}
+
+/**
+ * Very simple tree scoring function.  True if the point is inside a triangle.
+ */
+fun insideTree(p: Point): Boolean {
+    val (x, y) = p
+    val x0 = if (x < 52) x else 103 - x
+    return y > (52 - x0) * 18 / 10
 }
 
 data class Robot(
@@ -63,3 +89,46 @@ fun Point.toQuadrant(width: Int, height: Int): Int? {
     }
 }
 
+class Display private constructor(
+    val width: Int,
+    val height: Int,
+    val frame: JFrame,
+    val panel: Panel,
+) {
+    companion object Factory {
+        fun create(width: Int, height: Int): Display {
+            val frame = JFrame("Day 14 - Robots")
+            frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+            frame.setSize(width * 4, height * 4)
+            val panel = Panel(width, height, setOf())
+            frame.add(panel)
+            frame.isVisible = true
+            return Display(width, height, frame, panel)
+        }
+    }
+
+    fun update(robots: Set<Point>) {
+        panel.update(robots)
+        frame.repaint()
+    }
+}
+
+class Panel(
+    val w: Int,
+    val h: Int,
+    var robots: Set<Point>
+) : JPanel(true) {
+    override fun paintComponent(g: java.awt.Graphics) {
+        g.color = Color.GRAY
+        g.fillRect(0, 0, w * 4, h * 4)
+
+        g.color = Color.GREEN
+        for ((x, y) in robots) {
+            g.fillRect(x * 4, y * 4, 4, 4)
+        }
+    }
+
+    fun update(robots: Set<Point>) {
+        this.robots = robots
+    }
+}
