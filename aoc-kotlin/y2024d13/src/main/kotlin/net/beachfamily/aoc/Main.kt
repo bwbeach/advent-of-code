7fd8d18ba3f5dd09c636194466403ce48e007302
Brian Beach <coder@beachfamily.net>
2024-12-19 14:04:12 -1000

    y2024d13 part 2

--- aoc-kotlin/y2024d13/src/main/kotlin/net/beachfamily/aoc/Main.kt
+++ aoc-kotlin/y2024d13/src/main/kotlin/net/beachfamily/aoc/Main.kt
@@ -1,17 +1,30 @@
 package net.beachfamily.aoc
 
+import java.math.BigInteger
+
 fun main() {
     val input = readInput("y2024d13")
     println(part1(input))
     println(part2(input))
 }
 
-fun part1(s: String) : Int {
+fun part1(s: String) : BigInteger {
     return parseProblems(s)
         .mapNotNull { it.solve() }
-        .map { (a, b) -> a * 3 + b }
+        .map { (a, b) -> a * 3.toBigInteger() + b }
         .sum()
 }
+
+fun part2(s: String) : BigInteger {
+    return parseProblems(s)
+        .map { it.toPart2Problem() }
+        .mapNotNull { it.solve() }
+        .map { (a, b) -> a * 3.toBigInteger() + b }
+        .sum()
+}
+
+fun Sequence<BigInteger>.sum(): BigInteger =
+    this.fold(BigInteger.ZERO) { acc, i -> acc + i }
 
 fun parseProblems(s : String): Sequence<Problem> {
     return lines(s)
@@ -25,33 +38,45 @@ fun parseProblemInput(input: String): Problem {
     val matchResult = regex.find(input) ?: throw IllegalArgumentException("Input format does not match the expected pattern.")
 
     val (ax, ay, bx, by, px, py) = matchResult.destructured
-    return Problem(ax.toInt(), ay.toInt(), bx.toInt(), by.toInt(), px.toInt(), py.toInt())
-}
-
-fun part2(s: String) : Int {
-    return s.length
+    return Problem(
+        ax.toBigInteger(),
+        ay.toBigInteger(),
+        bx.toBigInteger(),
+        by.toBigInteger(),
+        px.toBigInteger(),
+        py.toBigInteger()
+    )
 }
 
 data class Problem(
-    val ax: Int,
-    val ay: Int,
-    val bx: Int,
-    val by: Int,
-    val px: Int,
-    val py: Int,
+    val ax: BigInteger,
+    val ay: BigInteger,
+    val bx: BigInteger,
+    val by: BigInteger,
+    val px: BigInteger,
+    val py: BigInteger,
 ) {
-    fun solve(): Pair<Int, Int>? {
-        println("$this")
+    fun solve(): Pair<BigInteger, BigInteger>? {
         val det = (ax * by) - (ay * bx)
-        require(det != 0)
+        require(det != BigInteger.ZERO)
         val detA = (px * by) - (py * bx)
         val detB = (ax * py) - (ay * px)
-        println("$det $detA $detB ${detA % det} ${detB % det}")
-        if (detA % det != 0 || detB % det != 0) {
+        if (detA % det != BigInteger.ZERO || detB % det != BigInteger.ZERO) {
             return null
         } else {
             return Pair(detA / det, detB / det)
         }
+    }
+
+    fun toPart2Problem(): Problem {
+        return Problem(
+            ax,
+            ay,
+            bx,
+            by,
+            px + 10000000000000.toBigInteger(),
+            py + 10000000000000.toBigInteger()
+        )
     }
 }
 
