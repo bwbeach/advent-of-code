e5a08363304a2a3612eaa67d5492ffe6b03db24b
Brian Beach <coder@beachfamily.net>
2024-12-19 14:51:03 -1000

    y2024d14 part 1

--- aoc-kotlin/y2024d14/src/main/kotlin/net/beachfamily/aoc/Main.kt
+++ aoc-kotlin/y2024d14/src/main/kotlin/net/beachfamily/aoc/Main.kt
@@ -2,15 +2,64 @@ package net.beachfamily.aoc
 
 fun main() {
     val input = readInput("y2024d14")
-    println(part1(input))
+    println(part1(input, 101, 103))
     println(part2(input))
 }
 
-fun part1(s: String) : Int {
-    return s.length
+fun part1(s: String, width: Int, height: Int) : Int {
+    val robots = lines(s).map(::parseRobot)
+    val positions = robots.map { it.posAfter(100, width, height) }
+    val quadrants = positions.mapNotNull { it.toQuadrant(width, height) }
+    val quadrantCounts = quadrants.groupingBy { it }.eachCount()
+    println(quadrantCounts)
+    return quadrantCounts.values.product()
 }
+
+fun Iterable<Int>.product(): Int = fold(1) { acc, i -> acc * i }
 
 fun part2(s: String) : Int {
     return s.length
+}
+
+data class Robot(
+    val pos: Point,
+    val vel: Point,
+) {
+    fun posAfter(duration: Int, width: Int, height: Int) =
+        Point(
+            (pos.x + duration * vel.x).mod(width),
+            (pos.y + duration * vel.y).mod(height),
+        )
+}
+
+/**
+ * Parses a string and returns a Robot.
+ */
+fun parseRobot(s: String): Robot {
+    val regex = Regex("""p=(-?\d+),(-?\d+)\s+v=(-?\d+),(-?\d+)""")
+    val match = regex.find(s) ?: throw IllegalArgumentException("Invalid input format")
+
+    val (px, py, vx, vy) = match.destructured
+    return Robot(
+        pos = Point(px.toInt(), py.toInt()),
+        vel = Point(vx.toInt(), vy.toInt())
+    )
+}
+
+fun Point.toQuadrant(width: Int, height: Int): Int? {
+    require( x in 0 until width && y in 0 until height)
+    require(width % 2 == 1)
+    require(height % 2 == 1)
+    val midX = width / 2
+    val midY = height / 2
+    return when {
+        x == midX -> null
+        y == midY -> null
+        x < midX && y < midY -> 1
+        x < midX && y > midY -> 2
+        x > midX && y < midY -> 3
+        x > midX && y > midY -> 4
+        else -> throw IllegalArgumentException("bug")
+    }
 }
 
--- aoc-kotlin/y2024d14/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
+++ aoc-kotlin/y2024d14/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
@@ -5,9 +5,36 @@ import kotlin.test.assertEquals
 
 class MainTest {
 
+    fun testInput() =
+        """
+        p=0,4 v=3,-3
+        p=6,3 v=-1,-3
+        p=10,3 v=-1,2
+        p=2,0 v=2,-1
+        p=0,0 v=1,3
+        p=3,0 v=-2,-2
+        p=7,6 v=-1,-3
+        p=3,0 v=-1,-2
+        p=9,3 v=2,3
+        p=7,3 v=-1,2
+        p=2,4 v=2,-3
+        p=9,5 v=-3,-3
+        """.trimIndent()
+
+    @Test
+    fun testPosAfter() {
+        val robot = parseRobot("p=2,4 v=2,-3")
+        assertEquals(Point(2, 4), robot.posAfter(0, 11, 7))
+        assertEquals(Point(4, 1), robot.posAfter(1, 11, 7))
+        assertEquals(Point(6, 5), robot.posAfter(2, 11, 7))
+        assertEquals(Point(8, 2), robot.posAfter(3, 11, 7))
+        assertEquals(Point(10, 6), robot.posAfter(4, 11, 7))
+        assertEquals(Point(1, 3), robot.posAfter(5, 11, 7))
+    }
+
     @Test
     fun `examples from part1 problem statement`() {
-        assertEquals(4, part1("1122"))
+        assertEquals(12, part1(testInput(), 11, 7))
     }
 
     @Test
