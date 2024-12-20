2dba7c92875f622dc75f96c0c8116c7adfd6c440
Brian Beach <coder@beachfamily.net>
2024-12-20 13:03:25 -1000

    y2024d15 part 1

--- aoc-kotlin/y2024d15/src/main/kotlin/net/beachfamily/aoc/Main.kt
+++ aoc-kotlin/y2024d15/src/main/kotlin/net/beachfamily/aoc/Main.kt
@@ -7,10 +7,104 @@ fun main() {
 }
 
 fun part1(s: String) : Int {
-    return s.length
+    val (state, instructions) = parse(s)
+    for (c in instructions) {
+        state.push(c)
+    }
+    return state.score1()
 }
 
 fun part2(s: String) : Int {
     return s.length
 }
 
+fun parse(s: String): Pair<State, String> {
+    val (a, b) = lines(s).asSequence().splitBy { it.isEmpty() }.asPair()
+    val grid = gridOfChar(a.joinToString("\n"))
+    val instructions = b.joinToString("").filter { !it.isWhitespace()}
+    return Pair(State.Factory.create(grid), instructions)
+}
+
+class State(
+    val xmin: Int,
+    val xmax: Int,
+    val ymin: Int,
+    val ymax: Int,
+    // A map containing what's where
+    val contents: MutableMap<Point, Char>,
+    // Where is the robot?
+    var robotPos: Point,
+) {
+    companion object Factory {
+        fun create(grid: Grid<Char>): State =
+            State(
+                grid.xmin,
+                grid.xmax,
+                grid.ymin,
+                grid.ymax,
+                grid.data
+                    .toMutableMap(),
+                grid.data.entries
+                    .filter { it.value == '@'}
+                    .map { it.key }
+                    .first()
+            )
+    }
+
+    fun push(instr: Char) = pushFrom(robotPos, instructionToDir(instr))
+
+    fun pushFrom(pos: Point, dir: Point) {
+        val next = pos + dir
+        val what = contents[next]!!
+
+        // can't push into a wall
+        if (what == '#') {
+            return
+        }
+
+        // try pushing non-empty
+        if (what != '.') {
+            pushFrom(next, dir)
+        }
+
+        // if there was already a space, or we just made one, we can
+        // push what's in this spot
+        if (contents[next] == '.') {
+            contents[next] = contents[pos]!!
+            contents[pos] = '.'
+            if (pos == robotPos) {
+                robotPos = next
+            }
+        }
+    }
+
+    fun score1(): Int {
+        return contents.entries
+            .filter { it.value == 'O' }
+            .map { it.key.gps() }
+            .sum()
+    }
+
+    override fun toString(): String =
+        buildString {
+            for (y in ymin..ymax) {
+                for (x in xmin..xmax) {
+                    append(contents[Point(x, y)])
+                }
+                appendLine()
+            }
+        }
+}
+
+fun instructionToDir(c: Char): Point =
+    when (c) {
+        '<' -> Point(-1, 0)
+        '>' -> Point(1, 0)
+        '^' -> Point(0, -1)
+        'v' -> Point(0, 1)
+        else -> throw IllegalArgumentException("Bad instruction: $c")
+    }
+
+fun Point.gps(): Int {
+    return x + y * 100
+}
--- aoc-kotlin/y2024d15/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
+++ aoc-kotlin/y2024d15/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
@@ -5,9 +5,49 @@ import kotlin.test.assertEquals
 
 class MainTest {
 
+    fun smallTestInput() =
+        """
+        ########
+        #..O.O.#
+        ##@.O..#
+        #...O..#
+        #.#.O..#
+        #...O..#
+        #......#
+        ########
+
+        <^^>>>vv<v>>v<<
+        """.trimIndent()
+
+    fun bigTestInput() =
+        """
+        ##########
+        #..O..O.O#
+        #......O.#
+        #.OO..O.O#
+        #..O@..O.#
+        #O#..O...#
+        #O..O..O.#
+        #.OO.O.OO#
+        #....O...#
+        ##########
+
+        <vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+        vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+        ><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+        <<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+        ^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+        ^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+        >^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+        <><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+        ^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+        v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
+        """.trimIndent()
+
     @Test
     fun `examples from part1 problem statement`() {
-        assertEquals(4, part1("1122"))
+        assertEquals(2028, part1(smallTestInput()))
+        assertEquals(10092, part1(bigTestInput()))
     }
 
     @Test
