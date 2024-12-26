01a9951fac23ba5156836538ba08cda99f43728b
Brian Beach <coder@beachfamily.net>
2024-12-25 17:30:04 -1000

    y2024d22 part 1

--- aoc-kotlin/y2024d22/src/main/kotlin/net/beachfamily/aoc/Main.kt
+++ aoc-kotlin/y2024d22/src/main/kotlin/net/beachfamily/aoc/Main.kt
@@ -6,11 +6,22 @@ fun main() {
     println(part2(input))
 }
 
-fun part1(s: String) : Int {
-    return s.length
-}
+fun part1(s: String) : Long =
+    lines(s)
+        .map { it.toLong() }
+        .map { generateSequence(it) { n -> n.nextSecret() } }
+        .sumOf { it.drop(2000).first() }
 
 fun part2(s: String) : Int {
     return s.length
 }
+
+fun Long.nextSecret(): Long {
+    val a = this.mix(this.shl(6))
+    val b = a.mix(a.shr(5))
+    return b.mix(b.shl(11))
+}
+
+fun Long.mix(b: Long): Long =
+    (this.xor(b)).and(0xFFFFFF)
 
--- aoc-kotlin/y2024d22/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
+++ aoc-kotlin/y2024d22/src/test/kotlin/net/beachfamily/aoc/MainTest.kt
@@ -5,13 +5,43 @@ import kotlin.test.assertEquals
 
 class MainTest {
 
+    fun testInput() =
+        """
+        1
+        10
+        100
+        2024
+        """.trimIndent()
+
+    @Test
+    fun testNextSecret() {
+        val expectedSequence: Sequence<Long> = sequenceOf(
+            123,
+            15887950,
+            16495136,
+            527345,
+            704524,
+            1553684,
+            12683156,
+            11100544,
+            12249484,
+            7753432,
+            5908254,
+        )
+        for ((a, b) in expectedSequence.zipWithNext()) {
+            assertEquals(b, a.nextSecret())
+        }
+    }
+
     @Test
     fun `examples from part1 problem statement`() {
-        assertEquals(4, part1("1122"))
+        assertEquals(37327623, part1(testInput()))
     }
 
     @Test
     fun `examples from part2 problem statement`() {
         assertEquals(4, part2("1212"))
     }
+
+
 }
